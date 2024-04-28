bd4f9fd7cd32b76201266a403964fb2ab383af26
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-04-28 04:41:36 +0100

    Added class icon to legend card.

--- app/src/main/java/com/example/apexdiceroll/ui/LegendCard.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/LegendCard.kt
@@ -3,19 +3,24 @@ package com.example.apexdiceroll.ui
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +30,7 @@ import com.example.apexdiceroll.R
 fun LegendCard(
     priorityNo: Int,
     legendName: String,
+    @DrawableRes classIcon: Int,
     @DrawableRes legendIcon: Int
 ) {
     Card {
@@ -43,10 +49,21 @@ fun LegendCard(
 
             Spacer(modifier = Modifier.size(2.dp))
 
-            Text(
-                text = legendName,
-                style = MaterialTheme.typography.titleLarge
-            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(classIcon),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+
+                Spacer(modifier = Modifier.size(8.dp))
+
+                Text(
+                    text = legendName,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+
 
             Spacer(modifier = Modifier.size(24.dp))
         }
@@ -59,6 +76,7 @@ fun LegendCardPreview() {
     LegendCard(
         priorityNo = 1,
         legendName = "Valkyrie",
-        legendIcon = R.drawable.valk
+        legendIcon = R.drawable.valk,
+        classIcon = R.drawable.class_skirmisher
     )
 }
