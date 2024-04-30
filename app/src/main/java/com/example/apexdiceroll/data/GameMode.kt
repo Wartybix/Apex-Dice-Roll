5ee54974b5a1fc4648472f4cbc0716c8e7f84cd4
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-04-30 19:25:16 +0100

    Added game mode switcher UI

---
+++ app/src/main/java/com/example/apexdiceroll/data/GameMode.kt
@@ -0,0 +1,6 @@
+package com.example.apexdiceroll.data
+
+enum class GameMode(val modeName: String) {
+    BR("Battle Royale"),
+    Mixtape("Mixtape")
+}
---
+++ app/src/main/java/com/example/apexdiceroll/ui/components/GameModeSwitcher.kt
@@ -0,0 +1,55 @@
+package com.example.apexdiceroll.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.apexdiceroll.data.GameMode
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GameModeSwitcher(
+    selectedGameMode: GameMode,
+    onSwitch: (GameMode) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Game Mode", style = MaterialTheme.typography.titleSmall)
+        val gameModes = GameMode.entries
+
+        SingleChoiceSegmentedButtonRow {
+            gameModes.forEachIndexed { index, gameMode ->
+                SegmentedButton(
+                    selected = gameMode == selectedGameMode,
+                    onClick = { onSwitch(gameMode) },
+                    shape = SegmentedButtonDefaults.itemShape(index = index, count = gameModes.size)
+                ) {
+                    Text(text = gameMode.modeName)
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GameModeSwitcherPreviewBR() {
+    GameModeSwitcher(selectedGameMode = GameMode.BR, onSwitch = {})
+}
+
+@Preview
+@Composable
+fun GameModeSwitcherPreviewMixtape() {
+    GameModeSwitcher(selectedGameMode = GameMode.Mixtape, onSwitch = {})
+}
