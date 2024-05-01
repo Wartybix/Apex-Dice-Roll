66576df18555a1ebbe41755f34ecb0afea2555f0
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-01 10:42:36 +0100

    Enabled edge-to-edge appearance.

--- app/src/main/java/com/example/apexdiceroll/MainActivity.kt
+++ app/src/main/java/com/example/apexdiceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.apexdiceroll
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,6 +14,7 @@ import com.example.apexdiceroll.ui.theme.ApexDiceRollTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             ApexDiceRollTheme {
