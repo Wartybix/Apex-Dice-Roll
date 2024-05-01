3ffeb4c0da34c406da15ea642cbd957b3a3f7154
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-01 10:11:33 +0100

    Improved mixtape loadout display formatting.

--- app/src/main/java/com/example/apexdiceroll/ui/components/MixtapeLoadoutDisplay.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/components/MixtapeLoadoutDisplay.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.apexdiceroll.R
@@ -49,12 +50,13 @@ fun MixtapeLoadoutDisplay(
                 if (loadout == selectedLoadout) {
                     Surface(
                         shape = MaterialTheme.shapes.large,
-                        color = MaterialTheme.colorScheme.secondary,
+                        color = MaterialTheme.colorScheme.surfaceVariant,
                         modifier = Modifier.weight(1f)
                     ) {
                         Text(
                             text = selectedLoadout.loadoutName,
                             style = MaterialTheme.typography.titleLarge,
+                            textAlign = TextAlign.Center,
                             modifier = Modifier.padding(16.dp)
                         )
                     }
@@ -62,7 +64,7 @@ fun MixtapeLoadoutDisplay(
                 else {
                     Surface(
                         shape = CircleShape,
-                        color = MaterialTheme.colorScheme.surfaceVariant,
+                        color = MaterialTheme.colorScheme.surfaceContainer,
                         modifier = Modifier.size(24.dp)
                     ) {
 
