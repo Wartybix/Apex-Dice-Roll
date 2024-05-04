c29688e9892ca8e3683620f5618ca212236a6f99
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-04 05:06:32 +0100

    Changed Upgrade UI to use arrows instead of a checkmark.

--- app/src/main/java/com/example/apexdiceroll/ui/components/diceroll_screen/upgrades_area/Upgrade.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/components/diceroll_screen/upgrades_area/Upgrade.kt
@@ -2,18 +2,18 @@ package com.example.apexdiceroll.ui.components.diceroll_screen.upgrades_area
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,9 +42,18 @@ fun Upgrade(
         modifier = modifier.fillMaxHeight()
     ) {
         if (currentSelection == desiredSelection) {
-            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val icon by remember { mutableStateOf(
+                    when (currentSelection) {
+                        UpgradeSelection.LEFT -> Icons.Default.ChevronLeft
+                        UpgradeSelection.RIGHT -> Icons.Default.ChevronRight
+                    }
+                ) }
                 Icon(
-                    imageVector = Icons.Default.Check,
+                    imageVector = icon,
                     contentDescription = currentSelection.description
                 )
             }
