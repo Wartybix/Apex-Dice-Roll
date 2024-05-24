8514be7e3c1e93dd8fe2e6dab1cdb74f0b97dbb6
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-24 11:07:41 +0100

    Made wins screen prototype compile.

--- app/src/main/java/com/example/apexdiceroll/ui/screens/OverallScreen.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/screens/OverallScreen.kt
@@ -2,10 +2,13 @@ package com.example.apexdiceroll.ui.screens
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Sync
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -13,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
@@ -46,16 +50,15 @@ fun OverallScreen(
                     }
                 }
                 Screen.WinHistory -> {
-		    ExtendedFloatingActionButton(
-			onClick = { /*TODO*/ },
-			icon = Icons.Default.Add,
-			text = "Add Win"
-		    )
-		    //TODO add fab height modifier
-		}
-		else -> {}
+                    ExtendedFloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    icon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) },
+                    text = { Text("Add Win") }
+                    )
+                    //TODO add fab height modifier
+                }
+		        else -> {}
             }
-
         },
         content = { it ->
             when (uiState.currentScreen) {
--- app/src/main/java/com/example/apexdiceroll/ui/screens/WinsScreen.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/screens/WinsScreen.kt
@@ -1,3 +1,7 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
 //TODO reference package and import
 
 @Composable
