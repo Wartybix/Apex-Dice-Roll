ca81b3e9adae80f6b65e589132e98b7551f3e6e1
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-24 09:35:05 +0100

    Moved legend classes into the right order.

--- app/src/main/java/com/example/apexdiceroll/data/LegendClass.kt
+++ app/src/main/java/com/example/apexdiceroll/data/LegendClass.kt
@@ -5,8 +5,8 @@ import com.example.apexdiceroll.R
 
 enum class LegendClass(val className: String, @DrawableRes val icon: Int) {
     Assault(className = "Assault", icon = R.drawable.class_assault),
-    Controller(className = "Controller", icon = R.drawable.class_controller),
-    Recon(className = "Recon", icon = R.drawable.class_recon),
     Skirmisher(className = "Skirmisher", icon = R.drawable.class_skirmisher),
+    Recon(className = "Recon", icon = R.drawable.class_recon),
     Support(className = "Support", icon = R.drawable.class_support),
+    Controller(className = "Controller", icon = R.drawable.class_controller),
 }
