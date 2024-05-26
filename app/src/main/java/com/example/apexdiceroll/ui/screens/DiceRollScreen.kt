0944cccc2e02bc523b90d803bae707bdd3c12b6a
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-26 11:06:24 +0100

    Removed title from game mode selector.

--- app/src/main/java/com/example/apexdiceroll/ui/screens/DiceRollScreen.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/screens/DiceRollScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
@@ -34,7 +33,6 @@ import com.example.apexdiceroll.data.UpgradeSelection
 import com.example.apexdiceroll.ui.components.diceroll_screen.GameModeSwitcher
 import com.example.apexdiceroll.ui.components.diceroll_screen.legend_display.LegendCarousel
 import com.example.apexdiceroll.ui.components.diceroll_screen.MixtapeLoadoutDisplay
-import com.example.apexdiceroll.ui.components.diceroll_screen.SectionTitle
 import com.example.apexdiceroll.ui.components.diceroll_screen.upgrades_area.UpgradesDisplay
 import com.example.apexdiceroll.ui.theme.ApexDiceRollTheme
 
@@ -73,7 +71,6 @@ fun DiceRollScreen(
             .verticalScroll(rememberScrollState())
             .padding(top = topPadding, bottom = bottomPadding)
     ) {
-        SectionTitle(text = "Game Mode")
         GameModeSwitcher(
             selectedGameMode = selectedGameMode,
             onSwitch = { onGameModeSwitch(it) },
