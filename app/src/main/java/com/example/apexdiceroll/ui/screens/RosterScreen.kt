2d3d2e4bc0d2ac02a1bee964cc211283e85722f4
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-26 12:47:09 +0100

    Each legend (and class heading) are now individual entries of the roster screen's lazy list -- rather than the entire class sections.

--- app/src/main/java/com/example/apexdiceroll/ui/screens/RosterScreen.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/screens/RosterScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -51,10 +50,10 @@ fun RosterScreen(
         LazyColumn(
             modifier = modifier.weight(1f)
         ) {
-            items(LegendClass.entries) { legendClass ->
-                Spacer(modifier = Modifier.height(16.dp))
+            LegendClass.entries.forEach { legendClass ->
+                item {
+                    Spacer(modifier = Modifier.height(16.dp))
 
-                Column {
                     val classColour by remember {
                         mutableStateOf(
                             when (legendClass) {
@@ -90,26 +89,26 @@ fun RosterScreen(
                     }
 
                     Spacer(modifier = Modifier.size(8.dp))
-
-                    Column {
-                        val classLegends = legends.filter { it.legendClass == legendClass }
-
-                        classLegends.forEachIndexed { index, legend ->
-                            if (index != 0) {
-                                HorizontalDivider()
-                            }
-                            LegendToggle(
-                                legendName = legend.name,
-                                wins = legend.wins,
-                                selected = legend.selected,
-                                onToggle = { legend.selected = it }
-                            )
-                        }
-                    }
                 }
 
+                val classLegends = legends.filter { it.legendClass == legendClass }
+
+                classLegends.forEachIndexed { index, legend ->
+                    item {
+                        if (index != 0)
+                            HorizontalDivider()
+
+                        LegendToggle(
+                            legendName = legend.name,
+                            wins = legend.wins,
+                            selected = legend.selected,
+                            onToggle = { legend.selected = it }
+                        )
+                    }
+                }
             }
         }
+
 
         HorizontalDivider()
 
