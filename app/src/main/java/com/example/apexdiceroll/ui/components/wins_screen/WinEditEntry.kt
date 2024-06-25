1a9f449b8edf730b7b537d5094d03f1ab59cb57e
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-06-25 14:55:17 +0100

    Set keyboard type for win entry to numerical input.

--- app/src/main/java/com/example/apexdiceroll/ui/components/wins_screen/WinEditEntry.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/components/wins_screen/WinEditEntry.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
@@ -20,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.apexdiceroll.R
@@ -70,7 +72,8 @@ fun WinEditEntry(
 
                 )
             },
-            label = { Text(text = stringResource(id = R.string.wins)) }
+            label = { Text(text = stringResource(id = R.string.wins)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
         Spacer(Modifier.size(8.dp))
