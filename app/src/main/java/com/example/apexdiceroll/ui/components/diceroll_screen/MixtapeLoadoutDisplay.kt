1e82f339d14a5cbc6ab241c0e2bbbd09cd377e15
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-06-28 22:02:37 +0100

    Optimised imports.

--- app/src/main/java/com/example/apexdiceroll/ui/components/diceroll_screen/MixtapeLoadoutDisplay.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/components/diceroll_screen/MixtapeLoadoutDisplay.kt
@@ -3,9 +3,6 @@ package com.example.apexdiceroll.ui.components.diceroll_screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
@@ -15,13 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.apexdiceroll.R
 import com.example.apexdiceroll.data.MixtapeLoadout
 
 @Composable
