f6a3771b835a4e07a83df949030c3f07d6947c6f
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-06-29 20:33:10 +0100

    Added details for most played legends.

--- app/src/main/java/com/example/apexdiceroll/ui/components/wins_screen/WinStatsTab.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/components/wins_screen/WinStatsTab.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.QueryStats
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,7 +21,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.apexdiceroll.R
@@ -112,7 +116,24 @@ fun WinStatsTab(
                         }
 
                         legendsSorted.forEachIndexed { index, legend ->
-                            Text(text = legend.name)
+                            Row {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(
+                                        id = legend.legendClass.icon
+                                    ),
+                                    contentDescription = legend.legendClass.className
+                                )
+                                Column {
+                                    Text(text = legend.name)
+                                    Text(
+                                        text = stringResource(
+                                            id = R.string.apex_wins, legend.wins,
+                                            if (legend.wins == 1) "" else "s"
+                                        )
+                                    )
+                                }
+                            }
+
                         }
                     },
                     modifier = Modifier.fillMaxWidth()
