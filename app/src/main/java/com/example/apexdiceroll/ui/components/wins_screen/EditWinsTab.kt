445af26bf42c129ab8ccf10db5eebadd90bff472
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-06-29 17:14:41 +0100

    Added tutorial messages to the edit wins tab.

--- app/src/main/java/com/example/apexdiceroll/ui/components/wins_screen/EditWinsTab.kt
+++ app/src/main/java/com/example/apexdiceroll/ui/components/wins_screen/EditWinsTab.kt
@@ -1,19 +1,27 @@
 package com.example.apexdiceroll.ui.components.wins_screen
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.apexdiceroll.R
 import com.example.apexdiceroll.data.Legend
 import com.example.apexdiceroll.data.LegendClass
 import com.example.apexdiceroll.ui.components.shared.LegendClassHeading
+import com.example.apexdiceroll.ui.components.shared.Section
 
 @Composable
 fun EditWinsTab(modifier: Modifier = Modifier, legends: List<Legend>) {
@@ -21,6 +29,36 @@ fun EditWinsTab(modifier: Modifier = Modifier, legends: List<Legend>) {
         modifier = modifier,
         contentPadding = PaddingValues(horizontal = 24.dp, vertical = 16.dp)
     ) {
+        item {
+            Text(
+                text = stringResource(R.string.how_to_find_legend_wins_tutorial),
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.size(16.dp))
+        }
+        item {
+            Section(
+                content = {
+                    Row {
+                        Icon(imageVector = Icons.Outlined.Info, contentDescription = null)
+                        Spacer(modifier = Modifier.size(16.dp))
+                        Text(
+                            text = stringResource(
+                                R.string.manually_added_win_warning,
+                                stringResource(id = R.string.add_win)
+                            ),
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                },
+                color = MaterialTheme.colorScheme.tertiaryContainer
+            )
+        }
+        item {
+            Spacer(Modifier.size(16.dp))
+        }
         LegendClass.entries.forEach { legendClass ->
             item {
                 LegendClassHeading(
